8378c6245144bbb5a6df0bd3a5f6f7f6508dad9e
DataDaveH <dhuber@berkeley.edu>
2017-08-13 17:37:26 -0700

    performance enhancement for read and write in preprocessDict.py

--- preprocessDict.py
+++ preprocessDict.py
@@ -1,9 +1,8 @@
 import os
 import time
 import sys
-import os.path
+import io
 import gzip
-import shelve
 from collections import defaultdict
 
 if len(sys.argv) != 2:
@@ -36,41 +35,49 @@ for key in dictFnamesByDate:
     for filename in dictFnamesByDate[key]:
         print filename
         start_time = time.time()
-        #i = 0
+        
         with gzip.open( filename, 'r') as fin:
-            for line in fin:
+            # BufferedReader is much faster than reading straight from the file
+            f = io.BufferedReader(fin)
+            for line in f:
                 tokens = line.split()
                 try:
+                    # increment the count
                     dictAggViews[tokens[0][0:2]][tokens[1]] += int(tokens[2])
                 except Exception as e:
+                    # there are poorly formatted rows here and there, just skip
                     pass
-                    #print "Exception: ", e
-                    #print "line:", tokens
-
-
-                #print(dictAggViews[tokens[0][0:2]])
-                #i += 1
-                #if i % 10 == 0: break
+        
         end_time=time.time() - start_time
-        print "\tTime to process: %s seconds\n" % str(end_time)
-
-    #print len(dictAggViews.keys())
-    #print len(dictAggViews['en'].keys())
-
-    # write back out to disk
-    with gzip.open( "aggregateviews-%s.gzAgg" % key, 'w') as fout:
+        print "Time to process: %s seconds" % str(end_time)
+    
+    # write back out to disk - a chunk at a time for performance
+    zip_start_time = time.time()
+    with gzip.open( "aggregateviews-%s.gz" % key, 'w') as fout:
         for lang in dictAggViews:
+            chunk = []
+            chunkIter = 0
+            chunkSize = 5000 # tested with a few values, maybe can be tuned further
             for page in dictAggViews[lang]:
-                #print(fout.filename)
-                #print ( lang + ' ' + page + ' ' + str(dictAggViews[lang][page]))
-                fout.write( lang + ' ' + page + ' ' + str(dictAggViews[lang][page]) + '\n')
-
+                chunk.extend( (str(lang + ' ' + page + ' ' + str(dictAggViews[lang][page])), ))
+                chunkIter += 1
+                if chunkIter > chunkSize:
+                    write( '\n'.join( chunk))
+                    chunkIter = 0
+                    chunk = []
+            # write out the last chunk
+            if chunkIter > 0:
+                fout.write( '\n'.join( chunk))
+                chunkIter = 0
+                chunk = []
+    print "\tTime to zip: %s seconds\n" % str(time.time() - zip_start_time)
+    
     # rename the original files
     for filename in dictFnamesByDate[key]:
         os.rename( filename, filename + "_proc")
 
     end_date_time = time.time() - time_date_start
-    print "\n\tTotal time for %s: %d seconds\n" % (key, end_date_time)
+    print "\tTotal time for %s: %d seconds\n\n" % (key, end_date_time)
 
 
 
